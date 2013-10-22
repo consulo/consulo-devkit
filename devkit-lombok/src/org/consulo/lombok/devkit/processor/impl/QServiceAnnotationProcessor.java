602d9c6b9cb389b94e8c6ab8dc3d3a6ffaf69eab
VISTALL <vistall.valeriy@gmail.com>
2013-10-22 03:31:19 +0300

    license

--- devkit-lombok/src/org/consulo/lombok/devkit/processor/impl/QServiceAnnotationProcessor.java
+++ devkit-lombok/src/org/consulo/lombok/devkit/processor/impl/QServiceAnnotationProcessor.java
@@ -1,12 +1,32 @@
+/*
+ * Copyright 2013 Consulo.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.consulo.lombok.devkit.processor.impl;
 
-import com.intellij.psi.*;
-import com.intellij.psi.impl.light.LightMethodBuilder;
-import com.intellij.psi.util.PsiTypesUtil;
+import java.util.List;
+
 import org.consulo.lombok.processors.LombokSelfClassProcessor;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiModifier;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.impl.light.LightMethodBuilder;
+import com.intellij.psi.util.PsiTypesUtil;
 
 /**
  * @author VISTALL
