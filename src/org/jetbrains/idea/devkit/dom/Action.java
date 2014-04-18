e268a09496a799a4e5564ef3ae9242c68b3d7e80
VISTALL <vistall.valeriy@gmail.com>
2014-04-18 11:29:05 +0300

    is secondary

--- src/org/jetbrains/idea/devkit/dom/Action.java
+++ src/org/jetbrains/idea/devkit/dom/Action.java
@@ -168,6 +168,8 @@ public interface Action extends DomElement
 	 */
 	GenericAttributeValue<Boolean> isInternal();
 
+	GenericAttributeValue<Boolean> isSecondary();
+
 	GenericAttributeValue<Boolean> isCanUseProjectAsDefault();
 
 	GenericAttributeValue<String> getRequireModuleExtensions();
