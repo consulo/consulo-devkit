4180701ab3bbcbf9be27a765388d635cd7d652e1
Valeriy Semenchuk <vistall.valeriy@gmail.com>
2018-01-07 11:28:29 +0300

    preparing for mavenize

---
+++ src/main/java/consulo/devkit/run/ConsuloDebuggerRunner.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2013-2016 consulo.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package consulo.devkit.run;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import com.intellij.debugger.DebugEnvironment;
+import com.intellij.debugger.DebuggerManagerEx;
+import com.intellij.debugger.engine.DebugProcessImpl;
+import com.intellij.debugger.engine.DebuggerUtils;
+import com.intellij.debugger.engine.JavaDebugProcess;
+import com.intellij.debugger.impl.DebuggerSession;
+import com.intellij.debugger.impl.GenericDebuggerRunner;
+import com.intellij.debugger.settings.DebuggerSettings;
+import com.intellij.debugger.ui.tree.render.BatchEvaluator;
+import com.intellij.execution.DefaultExecutionResult;
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.ExecutionResult;
+import com.intellij.execution.configurations.RemoteConnection;
+import com.intellij.execution.configurations.RunProfile;
+import com.intellij.execution.configurations.RunProfileState;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.ui.RunContentDescriptor;
+import com.intellij.xdebugger.XDebugProcess;
+import com.intellij.xdebugger.XDebugProcessStarter;
+import com.intellij.xdebugger.XDebugSession;
+import com.intellij.xdebugger.XDebuggerManager;
+import com.intellij.xdebugger.impl.XDebugSessionImpl;
+
+/**
+ * @author VISTALL
+ * @since 29.05.14
+ */
+public class ConsuloDebuggerRunner extends GenericDebuggerRunner
+{
+	@NotNull
+	@Override
+	public String getRunnerId()
+	{
+		return "ConsuloDebuggerRunner";
+	}
+
+	@Override
+	public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile)
+	{
+		return super.canRun(executorId, profile) && profile instanceof ConsuloRunConfigurationBase;
+	}
+
+	@Nullable
+	@Override
+	protected RunContentDescriptor createContentDescriptor(@NotNull RunProfileState state, @NotNull ExecutionEnvironment env) throws ExecutionException
+	{
+		String address = DebuggerUtils.getInstance().findAvailableDebugAddress(DebuggerSettings.SOCKET_TRANSPORT).address();
+		RemoteConnection connection = new RemoteConnection(true, "127.0.0.1", address, false);
+
+		ConsuloSandboxRunState consuloSandboxRunState = (ConsuloSandboxRunState) state;
+
+		consuloSandboxRunState.getJavaParameters().getVMParametersList().addParametersString(connection.getLaunchCommandLine());
+		return attachVirtualMachine(consuloSandboxRunState, env, connection, true);
+	}
+
+	@Override
+	@Nullable
+	protected RunContentDescriptor attachVirtualMachine(RunProfileState state, @NotNull ExecutionEnvironment env, RemoteConnection connection, boolean pollConnection) throws ExecutionException
+	{
+		DebugEnvironment environment = new ConsuloDebugEnvironment(env, (ConsuloSandboxRunState) state, connection, pollConnection);
+		final DebuggerSession debuggerSession = DebuggerManagerEx.getInstanceEx(env.getProject()).attachVirtualMachine(environment);
+		if(debuggerSession == null)
+		{
+			return null;
+		}
+		else
+		{
+			final DebugProcessImpl debugProcess = debuggerSession.getProcess();
+			if(!debugProcess.isDetached() && !debugProcess.isDetaching())
+			{
+				if(environment.isRemote())
+				{
+					debugProcess.putUserData(BatchEvaluator.REMOTE_SESSION_KEY, Boolean.TRUE);
+				}
+
+				return XDebuggerManager.getInstance(env.getProject()).startSession(env, new XDebugProcessStarter()
+				{
+					@NotNull
+					@Override
+					public XDebugProcess start(@NotNull XDebugSession session)
+					{
+						XDebugSessionImpl sessionImpl = (XDebugSessionImpl) session;
+						ExecutionResult executionResult = debugProcess.getExecutionResult();
+						sessionImpl.addExtraActions(executionResult.getActions());
+						if(executionResult instanceof DefaultExecutionResult)
+						{
+							sessionImpl.addRestartActions(((DefaultExecutionResult) executionResult).getRestartActions());
+							sessionImpl.addExtraStopActions(((DefaultExecutionResult) executionResult).getAdditionalStopActions());
+						}
+
+						return JavaDebugProcess.create(session, debuggerSession);
+					}
+				}).getRunContentDescriptor();
+			}
+			else
+			{
+				debuggerSession.dispose();
+				return null;
+			}
+		}
+	}
+}
