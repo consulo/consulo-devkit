93c5e553716fd7d0b9b3009cda07ba9da1c2c903
Valery Semenchuk <vistall.valeriy@gmail.com>
2018-09-02 13:04:45 +0300

    add @Inject inspection

---
+++ plugin/src/main/java/consulo/devkit/inspections/inject/NoInjectAnnotationInspection.java
@@ -0,0 +1,84 @@
+package consulo.devkit.inspections.inject;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+
+import com.intellij.codeInsight.AnnotationUtil;
+import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.lang.jvm.JvmModifier;
+import com.intellij.psi.JavaElementVisitor;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiMethod;
+import consulo.annotations.RequiredReadAction;
+import consulo.devkit.inspections.util.service.ServiceInfo;
+import consulo.devkit.inspections.util.service.ServiceLocator;
+
+/**
+ * @author VISTALL
+ * @since 2018-09-02
+ */
+public class NoInjectAnnotationInspection extends LocalInspectionTool
+{
+	private static class Visitor extends JavaElementVisitor
+	{
+		private final ProblemsHolder myHolder;
+
+		public Visitor(ProblemsHolder holder)
+		{
+			myHolder = holder;
+		}
+
+		@Override
+		@RequiredReadAction
+		public void visitClass(PsiClass aClass)
+		{
+			ServiceInfo serviceInfo = ServiceLocator.findImplementationService(aClass);
+			if(serviceInfo == null)
+			{
+				return;
+			}
+
+			PsiMethod[] constructors = aClass.getConstructors();
+			if(constructors.length == 0)
+			{
+				// default constructor
+				if(aClass.hasModifier(JvmModifier.PUBLIC))
+				{
+					return;
+				}
+			}
+			else
+			{
+				PsiMethod defaultConstructor = null;
+				for(PsiMethod constructor : constructors)
+				{
+					if(constructor.hasModifier(JvmModifier.PUBLIC) && constructor.getParameterList().getParametersCount() == 0)
+					{
+						defaultConstructor = constructor;
+					}
+
+					if(AnnotationUtil.isAnnotated(constructor, Inject.class.getName(), 0))
+					{
+						return;
+					}
+				}
+
+				if(constructors.length == 1 && defaultConstructor != null)
+				{
+					return;
+				}
+			}
+
+			myHolder.registerProblem(aClass.getNameIdentifier(), "Missed @Inject annotation");
+		}
+	}
+
+	@Nonnull
+	@Override
+	public PsiElementVisitor buildVisitor(@Nonnull ProblemsHolder holder, boolean isOnTheFly)
+	{
+		return new Visitor(holder);
+	}
+}
