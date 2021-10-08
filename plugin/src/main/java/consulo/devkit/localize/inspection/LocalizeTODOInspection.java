bc6da3192d01c23d3fd02e6873c447127968d1f8
VISTALL <vistall.valeriy@gmail.com>
2021-10-08 12:44:08 +0300

    add inspection for localizeTODO method

---
+++ plugin/src/main/java/consulo/devkit/localize/inspection/LocalizeTODOInspection.java
@@ -0,0 +1,56 @@
+package consulo.devkit.localize.inspection;
+
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.*;
+import consulo.annotation.access.RequiredReadAction;
+import consulo.localize.LocalizeValue;
+import org.jetbrains.idea.devkit.inspections.DevKitInspectionBase;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author VISTALL
+ * @since 08/10/2021
+ */
+public class LocalizeTODOInspection extends DevKitInspectionBase
+{
+	private static final String localizeTODO = "localizeTODO";
+
+	@Nonnull
+	@Override
+	public PsiElementVisitor buildVisitor(@Nonnull ProblemsHolder holder, boolean isOnTheFly)
+	{
+		return new JavaElementVisitor()
+		{
+			@Override
+			@RequiredReadAction
+			public void visitMethodCallExpression(PsiMethodCallExpression expression)
+			{
+				PsiReferenceExpression methodExpression = expression.getMethodExpression();
+
+				String referenceName = methodExpression.getReferenceName();
+				if(localizeTODO.equals(referenceName))
+				{
+					PsiElement localizeTODOMethod = methodExpression.resolve();
+					if(localizeTODOMethod == null)
+					{
+						return;
+					}
+
+					PsiElement parent = localizeTODOMethod.getParent();
+					if(parent instanceof PsiClass psiClass && LocalizeValue.class.getName().equals(psiClass.getQualifiedName()))
+					{
+						PsiExpression[] expressions = expression.getArgumentList().getExpressions();
+						if(expressions.length != 1)
+						{
+							return;
+						}
+						
+						holder.registerProblem(expressions[0], new TextRange(0, expressions[0].getTextLength()), "Localize TODO text");
+					}
+				}
+			}
+		};
+	}
+}
