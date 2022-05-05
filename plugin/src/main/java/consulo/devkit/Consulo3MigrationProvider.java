a0495694266b6464a7b8ab19dd44e1a83b717b9f
VISTALL <vistall.valeriy@gmail.com>
2022-05-05 18:06:05 +0300

    add migration map for Consulo 2->3

---
+++ plugin/src/main/java/consulo/devkit/Consulo3MigrationProvider.java
@@ -0,0 +1,20 @@
+package consulo.devkit;
+
+import com.intellij.refactoring.migration.PredefinedMigrationProvider;
+
+import javax.annotation.Nonnull;
+import java.net.URL;
+
+/**
+ * @author VISTALL
+ * @since 05-May-22
+ */
+public class Consulo3MigrationProvider implements PredefinedMigrationProvider
+{
+	@Nonnull
+	@Override
+	public URL getMigrationMap()
+	{
+		return getClass().getResource("Consulo2-Consulo3.xml");
+	}
+}
