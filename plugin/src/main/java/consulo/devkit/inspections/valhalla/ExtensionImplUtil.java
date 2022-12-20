bf80d8b11141f5677e52c63c8ec977fa2fb4260e
Valery Semenchuk <vistall.valeriy@gmail.com>
2022-12-20 17:12:13 +0400

    corrected logic for nested clasess )

--- plugin/src/main/java/consulo/devkit/inspections/valhalla/ExtensionImplUtil.java
+++ plugin/src/main/java/consulo/devkit/inspections/valhalla/ExtensionImplUtil.java
@@ -2,7 +2,6 @@ package consulo.devkit.inspections.valhalla;
 
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiModifier;
-import com.intellij.psi.util.PsiUtil;
 import consulo.annotation.access.RequiredReadAction;
 
 /**
@@ -14,7 +13,12 @@ public class ExtensionImplUtil
 	@RequiredReadAction
 	public static boolean isTargetClass(PsiClass psiClass)
 	{
-		if(psiClass.hasModifierProperty(PsiModifier.ABSTRACT) || psiClass.isInterface() || psiClass.isAnnotationType() || psiClass.isEnum() || psiClass.isRecord() || PsiUtil.isInnerClass(psiClass))
+		if(psiClass.hasModifierProperty(PsiModifier.ABSTRACT) || psiClass.isInterface() || psiClass.isAnnotationType() || psiClass.isEnum() || psiClass.isRecord())
+		{
+			return false;
+		}
+
+		if(psiClass.getContainingClass() != null)
 		{
 			return false;
 		}
