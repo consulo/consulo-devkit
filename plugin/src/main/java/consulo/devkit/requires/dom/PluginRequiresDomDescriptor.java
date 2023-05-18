61d1ad2a5105c97e3e90f1588bbef9d5ca6e950e
Valery Semenchuk <vistall.valeriy@gmail.com>
2023-05-18 12:47:45 +0400

    allow only file with name 'plugin-requires.xml' for descriptor

--- plugin/src/main/java/consulo/devkit/requires/dom/PluginRequiresDomDescriptor.java
+++ plugin/src/main/java/consulo/devkit/requires/dom/PluginRequiresDomDescriptor.java
@@ -4,20 +4,35 @@ import consulo.annotation.component.ExtensionImpl;
 import consulo.component.util.Iconable;
 import consulo.platform.base.icon.PlatformIconGroup;
 import consulo.ui.image.Image;
+import consulo.util.lang.StringUtil;
+import consulo.virtualFileSystem.VirtualFile;
+import consulo.xml.psi.xml.XmlFile;
 import consulo.xml.util.xml.DomFileDescription;
+
+import javax.annotation.Nonnull;
 
 /**
  * @author VISTALL
  * @since 28-Aug-22
  */
 @ExtensionImpl
-public class PluginRequiresDomDescriptor extends DomFileDescription<PluginRequires> {
-  public PluginRequiresDomDescriptor() {
-    super(PluginRequires.class, "consulo-plugin-requires");
-  }
+public class PluginRequiresDomDescriptor extends DomFileDescription<PluginRequires>
+{
+	public PluginRequiresDomDescriptor()
+	{
+		super(PluginRequires.class, "consulo-plugin-requires");
+	}
 
-  @Override
-  public Image getFileIcon(@Iconable.IconFlags int flags) {
-    return PlatformIconGroup.actionsImport();
-  }
+	@Override
+	public boolean isMyFile(@Nonnull XmlFile file)
+	{
+		VirtualFile vFile = file.getVirtualFile();
+		return vFile != null && StringUtil.equal(vFile.getNameSequence(), "plugin-requires.xml", false);
+	}
+
+	@Override
+	public Image getFileIcon(@Iconable.IconFlags int flags)
+	{
+		return PlatformIconGroup.actionsImport();
+	}
 }
