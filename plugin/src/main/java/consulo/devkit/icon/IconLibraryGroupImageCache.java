0d5cf9b45af7c175c3305e139e4f913a6ac99bb7
Valery Semenchuk <vistall.valeriy@gmail.com>
2023-07-06 17:11:54 +0400

    add resolving icons inside libraries

--- plugin/src/main/java/consulo/devkit/icon/IconLibraryGroupImageCache.java
+++ plugin/src/main/java/consulo/devkit/icon/IconLibraryGroupImageCache.java
@@ -9,15 +9,18 @@ import consulo.annotation.component.ServiceAPI;
 import consulo.annotation.component.ServiceImpl;
 import consulo.disposer.Disposable;
 import consulo.language.content.ProductionResourceContentFolderTypeProvider;
+import consulo.language.psi.PsiUtilCore;
 import consulo.language.psi.util.QualifiedName;
-import consulo.language.util.ModuleUtilCore;
 import consulo.module.Module;
 import consulo.module.content.ModuleRootManager;
 import consulo.project.Project;
 import consulo.ui.image.Image;
+import consulo.util.collection.ArrayUtil;
+import consulo.util.lang.Couple;
 import consulo.util.lang.Pair;
 import consulo.util.lang.StringUtil;
 import consulo.virtualFileSystem.VirtualFile;
+import consulo.virtualFileSystem.archive.ArchiveVfsUtil;
 import consulo.virtualFileSystem.event.BulkFileListener;
 import consulo.virtualFileSystem.event.VFileEvent;
 import jakarta.inject.Inject;
@@ -42,7 +45,7 @@ public class IconLibraryGroupImageCache implements Disposable {
     return project.getInstance(IconLibraryGroupImageCache.class);
   }
 
-  private final Map<String, Pair<Image, VirtualFile>> myImageCache = new ConcurrentHashMap<>();
+  private final Map<Couple<String>, Pair<Image, VirtualFile>> myImageCache = new ConcurrentHashMap<>();
 
   private final ProjectIconsAccessor myProjectIconsAccessor;
 
@@ -61,20 +64,23 @@ public class IconLibraryGroupImageCache implements Disposable {
   @Nullable
   @RequiredReadAction
   public Pair<Image, VirtualFile> getImage(@Nonnull PsiMethod psiMethod) {
-    String iconPath = getIconPath(psiMethod);
-    if (iconPath == null) {
+    Couple<String> iconRef = getIconRef(psiMethod);
+    if (iconRef == null) {
       return null;
     }
 
-    Pair<Image, VirtualFile> pair = myImageCache.computeIfAbsent(iconPath, s -> {
-      Pair<Image, VirtualFile> result = findImage(psiMethod, s);
+    Pair<Image, VirtualFile> pair = myImageCache.computeIfAbsent(iconRef, ref -> {
+      Pair<Image, VirtualFile> result = findImage(psiMethod, ref);
       return result == null ? Pair.empty() : result;
     });
     return pair == Pair.<Image, VirtualFile>empty() ? null : pair;
   }
 
+  /**
+   * @return pair icon path and name
+   */
   @RequiredReadAction
-  private String getIconPath(@Nonnull PsiMethod psiMethod) {
+  private Couple<String> getIconRef(@Nonnull PsiMethod psiMethod) {
     PsiClass containingClass = psiMethod.getContainingClass();
     if (containingClass == null) {
       return null;
@@ -102,42 +108,77 @@ public class IconLibraryGroupImageCache implements Disposable {
       return null;
     }
 
-    Module module = ModuleUtilCore.findModuleForPsiElement(containingClass);
-    if (module == null) {
-      return null;
-    }
+    String[] iconRefs = iconLink.split("(?=\\p{Upper})");
+    String iconId = ArrayUtil.getLastElement(iconRefs);
+    // remove last
+    iconRefs = ArrayUtil.remove(iconRefs, iconRefs.length - 1);
 
-    String iconPathNoExt = StringUtil.join(iconLink.split("(?=\\p{Upper})"), "/");
-
-    String libPath = "ICON-LIB/light/" + iconGroupDirectory;
-    return libPath + "/" + iconPathNoExt;
+    String iconPath = "ICON-LIB/light/" + iconGroupDirectory + "/" + StringUtil.join(iconRefs, "/");
+    return Couple.of(iconPath, iconId);
   }
 
   @RequiredReadAction
   @Nullable
-  private Pair<Image, VirtualFile> findImage(@Nonnull PsiMethod psiMethod, @Nonnull String iconPath) {
-    Module module = Objects.requireNonNull(ModuleUtilCore.findModuleForPsiElement(psiMethod));
-
-    VirtualFile[] contentFolderFiles = ModuleRootManager.getInstance(module)
-                                                        .getContentFolderFiles(it -> it == ProductionResourceContentFolderTypeProvider
-                                                          .getInstance());
-
-
-    for (VirtualFile contentFolderFile : contentFolderFiles) {
-      VirtualFile iconFile = contentFolderFile.findFileByRelativePath(iconPath + ".png");
-      if (iconFile == null) {
-        iconFile = contentFolderFile.findFileByRelativePath(iconPath + ".svg");
+  private Pair<Image, VirtualFile> findImage(@Nonnull PsiMethod psiMethod, @Nonnull Couple<String> iconRef) {
+    Module module = psiMethod.getModule();
+    VirtualFile[] contentFolders = VirtualFile.EMPTY_ARRAY;
+    if (module == null) {
+      VirtualFile holderFile = PsiUtilCore.getVirtualFile(psiMethod);
+      // get root jar file
+      holderFile = holderFile == null ? null : ArchiveVfsUtil.getVirtualFileForArchive(holderFile);
+      // get root jar as directory
+      holderFile = holderFile == null ? null : ArchiveVfsUtil.getArchiveRootForLocalFile(holderFile);
+      if (holderFile != null) {
+        contentFolders = new VirtualFile[]{holderFile};
       }
+    }
+    else {
+      contentFolders = ModuleRootManager.getInstance(module)
+                                        .getContentFolderFiles(it -> it == ProductionResourceContentFolderTypeProvider
+                                          .getInstance());
+    }
 
-      if (iconFile == null) {
+    for (VirtualFile contentFolderFile : contentFolders) {
+      VirtualFile iconHolderDir = contentFolderFile.findFileByRelativePath(iconRef.getFirst());
+      if (iconHolderDir == null) {
         continue;
       }
 
-      Image icon = myProjectIconsAccessor.getIcon(iconFile, psiMethod);
-      return icon == null ? null : Pair.create(icon, iconFile);
+      VirtualFile iconFile = findChildByName(iconHolderDir, iconRef.getSecond());
+      if (iconFile != null) {
+        Image icon = myProjectIconsAccessor.getIcon(iconFile, psiMethod);
+        return icon == null ? null : Pair.create(icon, iconFile);
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Try find icon by name without case, since methods was always lower case
+   */
+  @Nullable
+  private VirtualFile findChildByName(VirtualFile owner, String iconId) {
+    VirtualFile[] children = owner.getChildren();
+
+    VirtualFile pngIcon = null;
+    for (VirtualFile child : children) {
+      String name = child.getNameWithoutExtension();
+
+      if (!iconId.equalsIgnoreCase(name)) {
+        continue;
+      }
+
+      String extension = child.getExtension();
+      if ("svg".equals(extension)) {
+        return child;
+      }
+
+      if ("png".equals(extension)) {
+        pngIcon = child;
+      }
     }
 
-    return null;
+    return pngIcon;
   }
 
   @Override
