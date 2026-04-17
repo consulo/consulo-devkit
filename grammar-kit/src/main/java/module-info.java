5120c534680bdaff28410daa0f38f3d5f8807d75
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-17 20:21:56 +0300

    reduce ide.api dependencies

--- grammar-kit/src/main/java/module-info.java
+++ grammar-kit/src/main/java/module-info.java
@@ -3,6 +3,38 @@
  * @since 2023-02-10
  */
 module consulo.devkit.grammar.kit {
+    requires consulo.application.api;
+    requires consulo.code.editor.api;
+    requires consulo.color.scheme.api;
+    requires consulo.component.api;
+    requires consulo.datacontext.api;
+    requires consulo.document.api;
+    requires consulo.file.chooser.api;
+    requires consulo.file.editor.api;
+    requires consulo.language.api;
+    requires consulo.language.editor.api;
+    requires consulo.language.editor.refactoring.api;
+    requires consulo.language.editor.ui.api;
+    requires consulo.language.impl;
+    requires consulo.localize.api;
+    requires consulo.logging.api;
+    requires consulo.module.api;
+    requires consulo.navigation.api;
+    requires consulo.base.icon.library;
+    requires consulo.project.api;
+    requires consulo.project.ui.api;
+    requires consulo.ui.api;
+    requires consulo.ui.ex.api;
+    requires consulo.ui.ex.awt.api;
+    requires consulo.usage.api;
+    requires consulo.util.collection;
+    requires consulo.util.dataholder;
+    requires consulo.util.io;
+    requires consulo.util.lang;
+    requires consulo.virtual.file.system.api;
+    requires asm;
+    requires velocity.engine.core;
+
     requires consulo.devkit.grammar.kit.core;
     requires consulo.java.language.api;
     requires consulo.java.debugger.impl;
@@ -11,7 +43,6 @@ module consulo.devkit.grammar.kit {
 
     // TODO remove in future
     requires java.desktop;
-    requires consulo.ide.impl;
 
     exports consulo.devkit.grammarKit.impl;
     exports org.intellij.grammar.impl;
--- plugin/src/main/java/module-info.java
+++ plugin/src/main/java/module-info.java
@@ -3,15 +3,64 @@
  * @since 2023-02-10
  */
 open module consulo.devkit {
+    requires consulo.application.api;
+    requires consulo.application.content.api;
+    requires consulo.base.icon.library;
+    requires consulo.code.editor.api;
+    requires consulo.component.api;
+    requires consulo.configurable.api;
+    requires consulo.datacontext.api;
+    requires consulo.diff.api;
+    requires consulo.disposer.api;
+    requires consulo.document.api;
+    requires consulo.execution.api;
+    requires consulo.execution.debug.api;
+    requires consulo.file.chooser.api;
+    requires consulo.file.editor.api;
+    requires consulo.file.template.api;
+    requires consulo.find.api;
+    requires consulo.index.io;
+    requires consulo.language.api;
+    requires consulo.language.editor.api;
+    requires consulo.language.editor.refactoring.api;
+    requires consulo.language.editor.ui.api;
+    requires consulo.language.impl;
+    requires consulo.localize.api;
+    requires consulo.logging.api;
+    requires consulo.module.api;
+    requires consulo.module.content.api;
+    requires consulo.module.ui.api;
+    requires consulo.navigation.api;
+    requires consulo.platform.api;
+    requires consulo.process.api;
+    requires consulo.project.api;
+    requires consulo.project.ui.api;
+    requires consulo.ui.api;
+    requires consulo.ui.ex.api;
+    requires consulo.ui.ex.awt.api;
+    requires consulo.undo.redo.api;
+    requires consulo.util.collection;
+    requires consulo.util.io;
+    requires consulo.util.jdom;
+    requires consulo.util.lang;
+    requires consulo.util.xml.serializer;
+    requires consulo.virtual.file.system.api;
+
+    requires consulo.ide.api;
+
     requires consulo.java;
+    requires consulo.java.language.api;
+    requires consulo.java.language.impl;
+    requires consulo.java.execution.api;
+    requires consulo.java.debugger.impl;
+    requires consulo.java.coverage.impl;
+    requires consulo.java.execution.impl;
+
     requires com.intellij.xml;
     requires com.intellij.xml.html.api;
     requires org.jetbrains.plugins.yaml;
     requires com.intellij.properties;
     requires org.jetbrains.idea.maven;
-
-    requires consulo.java.coverage.impl;
-    requires consulo.java.execution.impl;
 
     requires org.yaml.snakeyaml;
     requires dtdparser;
@@ -20,7 +69,7 @@ open module consulo.devkit {
     requires java.compiler;
 
     requires com.ibm.icu;
-    
+
     requires xercesImpl;
 
     // TODO remove in future
